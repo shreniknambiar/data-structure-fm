a0bd5a3b348ffb8d2f99baa0dc97b8b507273b37
Shrenik Nambiar <shreniknambiar86@gmail.com>
2022-01-03 14:39:14 -0400

    removed tests

--- merge-sort.js
+++ merge-sort.js
@@ -36,11 +36,3 @@ const merge = (left, right) => {
     return results.concat(left, right);
 }
   
-  // unit tests
-  // do not modify the below code
-//   test.skip("merge sort", function () {
-//     const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
-//     const ans = mergeSort(nums);
-//     expect(ans).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-//   });
-  
