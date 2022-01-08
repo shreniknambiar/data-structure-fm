b32625780bcab87fbe24fafa0fef60ca3a33cba1
Shrenik Nambiar <shreniknambiar86@gmail.com>
2022-01-08 03:06:36 -0400

    removed tests

--- quick-sort.js
+++ quick-sort.js
@@ -35,12 +35,3 @@ function quickSort(nums) {
     return leftSorted.concat(pivot, rightSorted);
   }
   
-  // unit tests
-  // do not modify the below code
-//   test.skip("quickSort", function () {
-//     const input = [10, 8, 2, 1, 6, 3, 9, 4, 7, 5];
-//     const answer = quickSort(input);
-  
-//     expect(answer).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-//   });
-  
